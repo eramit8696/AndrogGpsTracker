0a6e2520af9777c1ea12818c051ac65c8daea3ff
eramit8696 <er.amit8696@gmail.com>
2017-01-10 03:29:15 +0530

    Added Initial Version of GpS Android

---
+++ app/src/main/java/com/devidarshan/android/gpstracker/MainActivity.java
@@ -0,0 +1,72 @@
+package com.devidarshan.android.gpstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.location.Location;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private final String LOG_TAG = "LocationApp";
+    private TextView txtOutput;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        txtOutput = (TextView) findViewById(R.id.txtOutput);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(100);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (LocationListener) this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e("LOG_TAG", location.toString());
+        txtOutput.setText(Double.toString(location.getLatitude()));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("LOG_TAG", " Connection Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e("LOG_TAG", " Not Connectionted");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+        Log.e("Start", " Method");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.e("Stop", " Method");
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+}
